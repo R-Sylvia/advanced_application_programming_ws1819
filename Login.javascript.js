4d313149b547a89e3ae998d8553991880a61bc3b
Damoon <33068820+DAM8665@users.noreply.github.com>
2018-10-29 04:25:25 +0100

    Add files via upload

---
+++ Login.javascript.js
@@ -0,0 +1,36 @@
+
+
+var objectpeople = [
+
+    {      username: "DAM",
+           password: "Arab"
+            
+    },
+    
+    {      username: "Saeed",
+           password: "Aghaai"
+    
+    },
+    
+    {      username: "Rouz",
+           password: "Raf"
+    
+    }
+    ]
+     function getinfo() {
+    
+         var username = document.getElementById("username").value
+         var password = document.getElementById("password").value
+    
+          for (i = 0; i < objectpeople.length; i++) {
+             if (username == objectpeople[i].username && password == objectpeople[i].password) {
+                 console.log(username + "is logged in !!!")
+                 alert(username + "is logged in !!!");
+                 return
+              } 
+              else{
+                console.log("incorrect username or password")
+              }
+              
+          }
+     }
