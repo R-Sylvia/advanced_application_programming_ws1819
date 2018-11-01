a9271611eae243ea95950f2c69472269b373fcb5
Sylvia <sylvia_1994@hotmail.de>
2018-11-01 22:36:10 +0100

    included some of annas pages

---
+++ public/javascripts/canvas.js
@@ -0,0 +1,119 @@
+// JavaScript source code
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.height -= 87;
+
+var c = canvas.getContext('2d');
+
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+
+var click = {
+    x: undefined,
+    y: undefined
+}
+
+var MaxRadius = 50;
+var MinRadius = 20;
+
+/* ****************************** */
+/* ******* EVENT LISTENER ******* */
+/* ****************************** */
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener('click', function (event) {
+    click.x = event.x;
+    click.y = event.y;
+    //console.log(click);
+})
+
+
+/* ****************************** */
+/* *** Create and draw circles ** */
+/* ****************************** */
+function Circle(x, y, dx, dy, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color
+
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = this.color;
+        c.stroke();
+    }
+
+    this.update = function () {
+        if ((this.x + this.radius > innerWidth) || (this.x - this.radius < 0)) {
+            this.dx = -this.dx;
+        }
+
+        if ((this.y + this.radius > innerHeight) || (this.y - this.radius < 0)) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        // interactivity with mouse hover event
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.radius < MaxRadius) {
+                this.radius += 0.7;
+            }
+        }
+        else if (this.radius > MinRadius) {
+            this.radius -= 1;
+        }
+
+        // interactivity with click event
+        if (click.x - this.x < 20 && click.x - this.x > -20 && click.y - this - y < 60 && click.y - this.y > -60) {
+            console.log(click);
+            //c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        }
+        this.draw();
+    }
+}
+
+/* ****************************** */
+/* *** Calculate random color *** */
+/* ****************************** */
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
+}
+
+// Array of circle objects
+var circleArray = [];
+
+// fill out the array with random circles
+for (var i = 0; i < 150; i++) {
+    var radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
+    var color = random_rgba();
+
+    circleArray.push(new Circle(x, y, dx, dy, radius, color));
+}
+
+/* ****************************** */
+/* *** animate the canvas page ** */
+/* ****************************** */
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+}
+animate();
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ let path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-   // res.sendFile(path.join(__dirname, '/../public/client.html'));
+  //res.render('index', { title: 'Express' });
+  res.sendFile(__dirname + '/index.html');
 });
 
 router.use(express.static("public"));
