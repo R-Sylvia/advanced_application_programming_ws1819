34dd8c6e4e26c3da4fa94107216011efee382c89
Sylvia <sylvia_1994@hotmail.de>
2018-11-01 21:39:02 +0100

    chat example works now

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -19,6 +19,18 @@ ws.onopen = function () {
     ws.send('hello server');
 };*/
 
+$(function () {
+    //var socket = io();
+    $('form').submit(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
+});
+
 
 
 
